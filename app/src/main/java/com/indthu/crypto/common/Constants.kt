6fae4f7eb1691d399007edbccef67b8c83260b21
Indthujan <indthujan@nets.com.sg>
2022-12-21 15:00:50 +0800

    list screen and detail screen implemented

--- app/src/main/java/com/indthu/crypto/common/Constants.kt
+++ app/src/main/java/com/indthu/crypto/common/Constants.kt
@@ -3,5 +3,5 @@ package com.indthu.crypto.common
 object Constants {
 
     const val BASE_URL = "https://api.coinpaprika.com/"
-    const val COIN_ID = "coin_id"
+    const val COIN_ID = "coinId"
 }
--- app/src/main/java/com/indthu/crypto/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/indthu/crypto/presentation/coin_detail/CoinDetailScreen.kt
@@ -2,16 +2,24 @@ package com.indthu.crypto.presentation.coin_detail
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.flowlayout.FlowRow
 import com.indthu.crypto.presentation.coin_detail.components.CoinDetailViewModel
+import com.indthu.crypto.presentation.coin_detail.components.CoinTag
+import com.indthu.crypto.presentation.coin_detail.components.TeamListItem
 
 @Composable
 fun CoinDetailScreen(
@@ -29,8 +37,56 @@ fun CoinDetailScreen(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-
+                        Text(
+                            text = "${coin.rank}. ${coin.name}. (${coin.symbol})",
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.weight(8f)
+                        )
+                        Text(
+                            text = if (coin.isActive) "Active" else "inActive",
+                            color = if (coin.isActive) Color.Green else Color.Red,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .align(CenterVertically)
+                                .weight(2f)
+                        )
                     }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = coin.description,
+                        style = MaterialTheme.typography.body2,
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.h3
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin.tags.forEach { tags ->
+                            CoinTag(tag = tags)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Team Members",
+                        style = MaterialTheme.typography.h3
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                items(coin.team) { teamMember ->
+                    TeamListItem(
+                        teamMember = teamMember,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                    Divider()
                 }
             }
         }
